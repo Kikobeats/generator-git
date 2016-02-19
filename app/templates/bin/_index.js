b55ccabefebab25e4195793d0497aef434409b4c
Kiko Beats <josefrancisco.verdu@gmail.com>
2016-02-19 10:50:09 +0100

    improve cli

--- app/templates/bin/_index.js
+++ app/templates/bin/_index.js
@@ -1,13 +1,16 @@
 #!/usr/bin/env node
 'use strict';
 
+var path = require('path')
 var pkg = require('../package.json')
+
 require('update-notifier')({pkg: pkg}).notify()
 
 var <%= appname %> = require('./')
+
 var cli = require('meow')({
-  pkg: pkg
-  help: require('fs').readFileSync(__dirname + '/help.txt', 'utf8')
+  pkg: pkg,
+  help: require('fs').readFileSync(path.join(__dirname, 'help.txt'), 'utf8')
 })
 
 if (cli.input.length === 0) cli.showHelp()
